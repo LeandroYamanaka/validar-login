3dece19c99d281bb628b3ad749bdfd2ae07b9d36
Leandro Yamanaka <leandroyamanaka@gmail.com>
2022-12-22 15:26:43 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick, desativar }: IButtonProps) => {
+  return <ButtonContainer disabled={desativar} onClick={onClick}>{title}</ButtonContainer>;
+};
+
+
+export default Button;
